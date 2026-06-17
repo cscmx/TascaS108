2b36449cfb99b56017a8b46769ea1ec1d13d604f
cscmx <sainz.carolina@gmail.com>
2026-06-17 14:19:55 +0200

    Refactor: speed limmit-test empty functions created

---
+++ tests/speedRadarSensorTests.php
@@ -0,0 +1,32 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+require_once 'src/ejercicio2_SpeedRadarSensor.php';
+
+class SpeedRadarSensorTest extends TestCase
+{
+    public function testisTooSlow ()
+    {
+
+    }
+    public function testisAdecuate()
+    {
+
+    }
+    public function testisFast() 
+    {
+
+    }
+    public function testisTooFast()
+    {
+
+    }
+    public function testisDangerous() 
+    {
+        
+    }
+}
+
+
+
+?>
