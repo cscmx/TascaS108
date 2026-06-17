eadf0f2791bb2b032c3aacc4ba129ca82c606d6c
cscmx <sainz.carolina@gmail.com>
2026-06-17 12:55:32 +0200

    Refactor: tests de comprobación listos en numberCheckerTest.php

--- tests/numberCheckerTest.php
+++ tests/numberCheckerTest.php
@@ -1,10 +1,30 @@
 <?php
 
-use PHPUNIT\Framework\TestCase;
+use PHPUnit\Framework\TestCase;
 require_once 'src/numberChecker.php';
 
 class numberCheckerTest extends TestCase
 
 {
-    
+    public function testisEven() 
+    {    
+        $numberChecker = new numberChecker(4);
+        $resultado = $numberChecker->isEven();
+        $this->assertSame(true, $resultado);
+    }
+
+    public function testisPositive()
+    {
+        $numberChecker = new numberChecker(1);
+        $resultado = $numberChecker->isPositive();
+        $this->assertSame(true, $resultado);
+
+    }
+
+    public function testisZero()
+    {
+        $numberChecker = new numberChecker(0);
+        $resultado = $numberChecker->isPositive();
+        $this->assertSame(false, $resultado);
+    }
 }
